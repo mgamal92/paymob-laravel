611a74e1684cb9bc662fbf342461e6b84ea941ca
Mohamed Gamal <mg.dev1992@gmail.com>
2022-10-13 03:18:03 +0200

    make the TransactionCallbackController initializable

--- src/Controllers/TransactionCallbackController.php
+++ src/Controllers/TransactionCallbackController.php
@@ -6,19 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MG\Paymob\Paymob;
 
-abstract class TransactionCallbackController extends Controller
+class TransactionCallbackController extends Controller
 {
     /**
      * Handle Transaction Callback Processed
      * @param Request $request
      * @return void
      */
-    public abstract function processed(Request $request);
+    public function processed(Request $request)
+    {
+
+    }
 
     /**
      * Handle Transaction Callback Response
      * @param Request $request
      * @return void
      */
-    public abstract function response(Request $request);
+    public function response(Request $request)
+    {
+
+    }
 }
