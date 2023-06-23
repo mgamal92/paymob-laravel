7675040e63836928be7a4eb28f12de7b0daabd1c
H.H <Hamza.Hassan.dev@gmail.com>
2023-06-24 00:02:39 +0300

    [fix] Fix methods parameters type

--- src/Traits/Request.php
+++ src/Traits/Request.php
@@ -16,26 +16,26 @@ trait Request
         $this->base_url = config('paymob.base_url');
     }
 
-    private function getBaseUrl()
+    private function getBaseUrl(): string
     {
         return $this->base_url;
     }
 
-    public function get($url, $params = [])
+    public function get(string $url, array $params = [])
     {
         return $this->request('get', $url, $params);
     }
 
-    public function post($url, $params = [])
+    public function post(string $url, array $params = [])
     {
         return $this->request('post', $url, $params);
     }
 
-
-    private function request($method, $url, $params)
+    private function request(string $method, string $url, array  $params)
     {
         try {
-            return Http::$method($this->base_url.$url, $params)->json();
+            $fullUrl = $this->base_url . $url;
+            return Http::$method($fullUrl, $params)->json();
         } catch (Exception $e) {
             throw $e;
         }
